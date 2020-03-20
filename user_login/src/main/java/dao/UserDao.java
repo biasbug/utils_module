7f2f61efdb88ed8cb7e7357b4f7e3321c38f9b66
biasbug <xuhongda_code@163.com>
2020-03-20 15:44:06 +0800

    update

--- user_login/src/main/java/dao/UserDao.java
+++ user_login/src/main/java/dao/UserDao.java
@@ -1,4 +1,9 @@
 package dao;
 
-public class UserDao {
+import domain.UserBean;
+import org.apache.ibatis.annotations.Select;
+
+public interface UserDao {
+    @Select("select * from usertable where username = #{param1} and password = #{param2}")
+    public UserBean findUser(String username,String password);
 }
--- user_login/src/test/java/DaoTest.java
+++ user_login/src/test/java/DaoTest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import dao.UserDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/applicationContext-dao.xml")
 public class DaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void test(){
+
+        System.out.println(userDao.findUser("admin","12345678"));
+    }
 }
